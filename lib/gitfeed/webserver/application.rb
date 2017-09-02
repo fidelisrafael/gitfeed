f9872812db70ee0923894225b37908d6ad8d6f93
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2017-09-02 05:18:44 -0300

    Timeout changed to 3 minutes in \`POST /v1/users/:username/generate\` endpoint

--- lib/gitfeed/webserver/application.rb
+++ lib/gitfeed/webserver/application.rb
@@ -9,18 +9,22 @@ module GitFeed
   module WebServer
     # Simple `Rack` HTTP application
     class Application < BaseApplication
-      MAX_TIMEOUT = 60 # 60 seconds
+      MAX_TIMEOUT = 180 # 3 minutes
 
       post '/api/v1/users/:username/generate' do
         username = params[:username]
         force_refresh = params[:_force] == 'true'
+        verbose = params[:_vvv] == 'true'
 
         if username.nil? || username.empty?
+          # Bad Request
           return json({ error: true, reason: 'empty username' }, 400)
         end
 
         Timeout.timeout(MAX_TIMEOUT) do
-          CLI.run!(username, false, force_refresh)
+          # TODO: Don't allow to set `verbose` in production
+          # (must always be false)
+          CLI.run!(username, verbose, force_refresh)
         end
 
         json(ok: true)
