1a038b9338f6f432044dc5171ccae6af70bc7e09
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2017-09-02 04:30:17 -0300

    Added timeout to request response

--- lib/gitfeed/webserver/application.rb
+++ lib/gitfeed/webserver/application.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'timeout'
 require_relative 'base_application'
 
 require_relative '../cli'
@@ -8,11 +9,19 @@ module GitFeed
   module WebServer
     # Simple `Rack` HTTP application
     class Application < BaseApplication
+      MAX_TIMEOUT = 60 # 60 seconds
+
       post '/api/v1/users/:username/generate' do
         username = params[:username]
         force_refresh = params[:_force] == 'true'
 
-        CLI.run!(username, false, force_refresh)
+        if username.nil? || username.empty?
+          return json({ error: true, reason: 'empty username' }, 400)
+        end
+
+        Timeout.timeout(MAX_TIMEOUT) do
+          CLI.run!(username, false, force_refresh)
+        end
 
         json(ok: true)
       end
